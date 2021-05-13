01c8dffbda7cdb506dda79d61acf71fd3b53295f
copyNdpaste <julsnutini@naver.com>
2021-05-13 22:53:59 +0900

    [add] verify auth repo, test

--- tests/core/domains/user/repository/test_auth_repository.py
+++ tests/core/domains/user/repository/test_auth_repository.py
@@ -6,7 +6,7 @@ from core.domains.user.repository.auth_repository import AuthRepository
 from core.domains.user.repository.user_repository import UserRepository
 
 
-def test_when_user_signup_then_success(session):
+def test_create_auth_repo_then_success(session):
     UserRepository().signup(nickname="test_nickname", password="test_password")
 
     result = AuthRepository().create_auth(
@@ -18,3 +18,16 @@ def test_when_user_signup_then_success(session):
     )
 
     assert result
+
+
+def test_update_auth_repo_then_success(session):
+    UserRepository().signup(nickname="test_nickname", password="test_password")
+    AuthRepository().create_auth(
+        user_id=1,
+        identification="test@email.com",
+        type_="email",
+        verify_code="1234",
+        expired_at=get_utc_timestamp() + timedelta(minutes=3),
+    )
+
+    assert AuthRepository().update_auth(user_id=1, identification="test@email.com")
